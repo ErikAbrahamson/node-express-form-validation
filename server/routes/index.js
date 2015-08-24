41a04f271fbaa8324e40f47bc00976d41f31aa50
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-08-24 11:45:30 -0600

    Add validation

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,14 +9,38 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/submit', function(req, res, next) {
-  puppyArray.push({
-    name: req.body.puppyName,
-    id: req.body.puppyID
-  });
-  console.log(puppyArray);
-  res.render('puppies', {
-    puppies: puppyArray,
-    success: 'You\'ve just added a new puppy!'
-  });
+  var puppyInputName = req.body.puppyName;
+  var puppyInputID = req.body.puppyID;
+  var errors = puppyValidationCheck(puppyInputName, puppyInputID);
+  if (errors) {
+    res.render('index', {
+      title: 'Errors',
+      errors: errors
+    });
+  } else {
+    puppyArray.push({
+      name: puppyInputName,
+      id: puppyInputID
+    });
+    res.render('puppies', {
+      puppies: puppyArray,
+      success: 'You\'ve just added a new puppy!'
+    });
+  }
 });
+
+function puppyValidationCheck(puppyName, puppyID) {
+  var errorArray = [];
+  var puppyNameTrimmed = puppyName.trim();
+  var puppyIDTrimmed = puppyID.trim();
+  if(puppyNameTrimmed === '') {
+    errorArray.push("Name cannot be blank.");
+  }
+  if(puppyIDTrimmed === '') {
+    errorArray.push('ID cannot be blank.');
+  } else if (puppyIDTrimmed.length < 3) {
+    errorArray.push('An ID must be at least 3 characters long.');
+  }
+  return errorArray;
+}
 module.exports = router;
